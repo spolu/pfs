32c9c84e87ec7df1963c336dae188337d1997adb
Stanislas Polu <spolu@stanislas-stanford-edu.local>
2008-05-31 21:37:00 -0700

    lan_tun done

--- src/pfsd/lan_tun/lan_tun.py
+++ src/pfsd/lan_tun/lan_tun.py
@@ -11,8 +11,8 @@ sd_owner = sys.argv[2]
 sd_name = sys.argv[3]
 pfsd_port = int (sys.argv[4])
 tun_port = pfsd_port + 1
-timeout = 5
 new_port = tun_port + 1
+threads = []
 
 #######################
 
@@ -64,6 +64,7 @@ class DNSBrowse (Thread):
 
         for n, s in self.services.iteritems():
             s.stop ()
+        for n, s in self.services.iteritems():
             s.join ()
             print n, 'removed'
             
@@ -103,25 +104,41 @@ class Connect:
 
     def connect (self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((self.host,self.port))                
+        self.s.connect ((self.host,self.port))                
+
+    def setsock (self, s):
+        self.s = s
 
     def readline (self):
         buffer = ""
         data = self.s.recv (1)
         while data is not None:
             if data.startswith ('\n'):
+                print '<< ', buffer
+                return buffer
+            if data.startswith ('\r'):
+                data = self.s.recv (1)
+                print '<< ', buffer
                 return buffer
             buffer += str (data)
             data = self.s.recv (1)
+        print '<< ', buffer
         return buffer
 
     def writeline (self, str):
+        print '>> ', str
         self.s.send (str)
         self.s.send ('\n')
 
     def close (self):
-        print 'closing conn'
         self.s.close ()
+
+    def send (self, str):
+        self.s.send (str)
+    
+    def recv (self, len):
+        return self.s.recv (len)
+    
 
 
 
@@ -137,6 +154,8 @@ class SDevice (Thread):
         self.replyDomain = replyDomain
         self.resolved = False
         self.kill = False
+        self.threads = []
+        self.onlineSent = False
 
     def resolve_cb (self, sdRef, flags, interfaceIndex, errorCode,
                     fullname, hosttarget, port, txtRecord):
@@ -152,7 +171,7 @@ class SDevice (Thread):
                                                             callBack = self.query_cb)
         try:
             while not self.resolved:
-                ready = select.select ([self.query_sdRef], [], [], timeout)
+                ready = select.select ([self.query_sdRef], [], [], 5)
                 if self.query_sdRef not in ready[0]:
                     break
                 pybonjour.DNSServiceProcessResult (self.query_sdRef)
@@ -174,7 +193,7 @@ class SDevice (Thread):
                                                           self.replyDomain,
                                                           self.resolve_cb)
         while not self.resolved:
-            ready = select.select ([self.resolve_sdRef], [], [], timeout)
+            ready = select.select ([self.resolve_sdRef], [], [], 5)
             if self.resolve_sdRef not in ready[0]:
                 break
             pybonjour.DNSServiceProcessResult (self.resolve_sdRef)
@@ -187,27 +206,229 @@ class SDevice (Thread):
 
     def runLocalSDServer (self):
         global new_port
+        global tun_port
+        global pfsd_port
+        global threads
         if (new_port == tun_port + 98):
             new_port = tun_port + 1
         self.local_port = new_port
         new_port += 1
     
+        tun = Connect (self.remote_ip, self.remote_port)
+        try:
+            tun.connect ()
+            tun.writeline ('WHOIS')
+            self.remote_sd_id = tun.readline ()
+            self.remote_sd_owner = tun.readline ()
+            self.remote_sd_name = tun.readline ()
+            tun.close ()
+        except:
+            return
+
+        pfsd = Connect ('localhost', pfsd_port)
+        try:
+            pfsd.connect ()
+            pfsd.writeline ('ONLINE')
+            pfsd.writeline ('1')
+            pfsd.writeline (str (self.local_port))
+            pfsd.writeline (self.remote_sd_id)
+            pfsd.writeline (self.remote_sd_owner)
+            pfsd.writeline (self.remote_sd_name)
+            pfsd.writeline ('CLOSE')
+            ans = pfsd.readline ()
+            pfsd.close ()
+            self.onlineSent = True
+        except:
+            self.stop ()
         
+        try:
+            srv_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+            srv_sock.bind (('', self.local_port))
+            srv_sock.listen (5)
+        except:
+            self.stop ()
+
+        print 'Starting SDServer on port ', self.local_port, ' kill : ', self.kill
+        while not self.kill:
+            ready = select.select ([srv_sock], [], [], 1)
+            if srv_sock in ready[0]:
+                srv = SDService (srv_sock.accept (), 
+                                 self.remote_ip, 
+                                 self.remote_port,
+                                 self.remote_sd_id)
+                srv.start ()
+                self.threads.append (srv)
+        
+        for srv in self.threads:
+            srv.stop ()
+        for srv in self.threads:
+            srv.join ()
+        srv_sock.close ()
         
         
     def stop (self):
+        global pfsd_port
         self.kill = True
-            
+        pfsd = Connect ('localhost', pfsd_port)
+        try:
+            if self.onlineSent:
+                pfsd.connect ()
+                pfsd.writeline ('OFFLINE')
+                pfsd.writeline ('1')
+                pfsd.writeline (self.remote_sd_id)
+                ans = pfsd.readline ()
+                pfsd.writeline ('CLOSE')
+                ans = pfsd.readline ()
+                pfsd.close ()
+        except:
+            pass
+
+
+#######################
+
+class SDService (Thread):
+    def __init__ (self, (client, address), ip, port, sd_id):
+        Thread.__init__ (self)
+        self.client = client
+        self.address = address
+        self.kill = False
+        self.remote_ip = ip
+        self.remote_port = port
+        self.remote_sd_id = sd_id
+        print 'SDService from ', address
+
+    def run (self):        
+        global pfsd_port
+        try:
+            self.tun_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.tun_sock.connect ((self.remote_ip, self.remote_port))                
+
+            tun = Connect ('', 0)
+            tun.setsock (self.tun_sock)
+            tun.writeline ('CONNECT')
+            line = tun.readline ()
+            if not line == 'OK':
+                self.stop ()
+            else:
+                print 'Connection to tun ', self.remote_sd_id
+
+            while not self.kill:
+                ready = select.select ([self.client, self.tun_sock], [], [], 1)
+                if self.client in ready[0]:
+                    data = self.client.recv (4096)
+                    self.tun_sock.send (data)
+                elif self.tun_sock in ready[0]:
+                    data = self.tun_sock.recv (4096)
+                    self.client.send (data)
+                else:
+                    break
+        except:
+            pass
+
+        self.tun_sock.close ()
+        self.client.close ()                
+
+    def stop (self):
+        self.kill = True
+    
+        
+
 
 
 #######################
 
 class TUNServer(Thread):
-    def __init__ (self, port):
+    def __init__ (self):
         Thread.__init__ (self)
-        self.port = port
+        self.kill = False
+        self.threads = []
+
+    def run (self):
+        global tun_port
+        global sd_id
+        global sd_owner
+        global sd_name
+
+        try:
+            srv_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+            srv_sock.bind (('', tun_port))
+            srv_sock.listen (5)
+        except:
+            self.stop ()
+
+        print 'Starting TUNServer on port ', tun_port, ' kill : ', self.kill
+        while not self.kill:
+            ready = select.select ([srv_sock], [], [], 1)
+            if srv_sock in ready[0]:
+                srv = TUNService (srv_sock.accept ())
+                srv.start ()
+                self.threads.append (srv)
         
-    
+        for srv in self.threads:
+            srv.stop ()
+        for srv in self.threads:
+            srv.join ()
+        srv_sock.close ()
+
+    def stop (self):
+        self.kill = True
+
+
+
+#######################
+
+class TUNService (Thread):
+    def __init__ (self, (client, address)):
+        Thread.__init__ (self)
+        self.kill = False
+        self.client = client
+        self.address = address
+        print 'TUNService from ', address
+
+    def run (self):
+        global sd_id
+        global sd_owner
+        global sd_name
+        global pfsd_port
+        cli = Connect ('', 0)
+        cli.setsock (self.client)
+
+        try:
+            cmd = cli.readline ()
+            if cmd == 'WHOIS':
+                cli.writeline (sd_id)
+                cli.writeline (sd_owner)
+                cli.writeline (sd_name)
+
+            if cmd == 'CONNECT':
+                try:
+                    self.pfsd_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    self.pfsd_sock.connect (('localhost', pfsd_port))
+                except:
+                    cli.writeline ('FAIL')
+                    self.stop ()
+                cli.writeline ('OK')
+                
+                while not self.kill:
+                    ready = select.select ([self.client, self.pfsd_sock], [], [], 1)
+                    if self.client in ready[0]:
+                        data = self.client.recv (4096)
+                        self.pfsd_sock.send (data)
+                    elif self.pfsd_sock in ready[0]:
+                        data = self.pfsd_sock.recv (4096)
+                        self.client.send (data)
+                    else:
+                        break
+
+                self.pfsd_sock.close ()
+        except:
+            pass
+        self.client.close ()                
+
+    def stop (self):
+        self.kill = True
+
+
 
 #######################
 
@@ -215,16 +436,25 @@ if __name__ == "__main__":
 
     name = sd_owner + '.' + sd_name
     reg_thread = DNSReg (name, '_pfs._tcp', tun_port)
+    threads.append (reg_thread)
     reg_thread.start ()
+    
     browse_thread = DNSBrowse ('_pfs._tcp')
+    threads.append (browse_thread)
     browse_thread.start ()
+
+    tun_srv_thread = TUNServer ()
+    threads.append (tun_srv_thread)
+    tun_srv_thread.start ()
 
     try:
         while True:
-            time.sleep (1)
+            pass
     except KeyboardInterrupt:
         pass
 
-    reg_thread.stop ()
-    browse_thread.stop ()
+    for t in threads:
+        t.stop ()
+        t.join ()
+    
 
